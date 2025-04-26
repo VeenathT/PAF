05c993eabbd783057660bcf5b6aeff39f7fea234
VeenathT <veenaththamuditha@gmail.com>
2025-04-26 11:25:22 +0530

    Update table header in Graph component to change 'Quantity' to 'No of items'

--- frontend/src/components/Graph/Graph.js
+++ frontend/src/components/Graph/Graph.js
@@ -53,7 +53,7 @@ const Graph = () => {
                             Photo Name
                         </th>
                         <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase tracking-wider">
-                            Quantity
+                            No of items
                         </th>
                     </tr>
                 </thead>
