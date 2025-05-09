c8b73a4124c841f4164ded2e68d26db49f3d0909
Thamuditha Veenath <159011911+VeenathT@users.noreply.github.com>
2025-05-09 21:07:33 +0530

    Delete backend directory

--- backend/src/main/java/backend/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.repository;
-
-import backend.model.InventoryModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<InventoryModel,Long> {
-}
